b5a24ffd6642981c80f0e7bf79cc98aa9b4ad6bd
JamesJ2019 <31362629+JamesJ2019@users.noreply.github.com>
2017-12-15 08:04:28 -0500

    Backround

---
+++ Backround.java
@@ -0,0 +1,28 @@
+import java.awt.*;
+
+public class Backround
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    private int width;
+    private int height;
+    private int size;
+    private Color horizonColor;
+    private Color groundColor;
+    private Color sunColor;
+    public Backround(int width, int height){
+        this.width = width;
+        this.height = height;
+        horizonColor = Color.CYAN;
+        groundColor = Color.GREEN;
+        sunColor = Color.YELLOW;
+        x=0;
+        
+        
+    }
+    
+    public void drawBackround(Graphics g)
+    {
+        
+    }
+}
--- TestClass.java
+++ TestClass.java
@@ -9,15 +9,11 @@ public class TestClass
 {
    public static void main(String args[]){
        DrawingPanel panel = new DrawingPanel(800,800);
-       Graphics g = panel.getGraphics();
-       
+       Graphics g = panel.getGraphics(); 
        Dragon jen = new Dragon();
-       
        jen.drawDragon(g);
-       jen.attack(g);
-     
+       jen.attack(g);    
        System.out.println(jen);
-       
        Dragon james = new Dragon(300,300,2, Color.BLUE, 25,"fire");
        james.drawDragon(g);
        james.attack(g);
