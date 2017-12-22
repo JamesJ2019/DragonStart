9f0365d0ab48ef37a1367a300ee606977fe33fad
J <J@something.com>
2017-12-22 07:57:43 -0500

    Array List Fixed

--- TestClass.java
+++ TestClass.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 /**
  * Write a description of class TestClass here.
  *
@@ -12,7 +13,8 @@ public class TestClass
        DrawingPanel panel = new DrawingPanel(800,800);
        Graphics g = panel.getGraphics();
        ArrayList<Dragon> dragons = new ArrayList<Dragon>();
-       int num;  
+       int num; 
+       Scanner input = new Scanner(System.in);
     
     do{
         System.out.println("How many Dragons would you like to create");
@@ -21,26 +23,16 @@ public class TestClass
     while(num <= 0);
     
     for(int i = 1; i <= num; i++){
-        Dragon temp = new Dragon(90 * i + 25, 90 * i +50,1 , new Color(50 * i, 50 * i, 50 * i), "#" + i);
-        dragon.add(temp);
+        Dragon temp = new Dragon(90 * i + 25, 90 * i + 50,1 , new Color(50 * i, 50 * i, 50 * i),1, "#" + i);
+        dragons.add(temp);
     }
     
-    for(int i; i = 0; i < dragons.size() i++;){
+    for(int i = 0; i < dragons.size(); i++){
         dragons.get(i).drawDragon(g);
-        dragons.get(i).dragonSayHello(g);
+        /*dragons.get(i).dragonSayHello(g);*/
     }
 }
 }
 
-Dragon jen = new Dragon();
-       jen.drawDragon(g);
-       jen.attack(g);    
-       System.out.println(jen);
-       
-       Dragon james = new Dragon(300,300,2, Color.BLUE, 25,"fire");
-       james.drawDragon(g);
-       james.attack(g);
-       
-       Backround bop = new Backround();
-       bop.drawBackround(g, panel);
+
 
